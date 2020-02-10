f2bd20d36a6f3893a95d94e892c04051b92f40f0
John Davis <davisjp89@gmail.com>
2020-02-09 23:52:19 -0500

    Implement byte compilation for WAV template fields
    This commit gives the ability to encode WAV template
    field data into bytes

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,18 +1,45 @@
 use super::wave;
 use super::headers;
+use super::common::Compile;
 
+#[derive(Clone)]
 pub struct WAVTemplate {
-    wave_header : headers::WaveHeader,
-    wave_format : headers::WaveFmt,
-    wave_data : headers::WaveData,
+    pub wave_header : headers::WaveHeader,
+    pub wave_format : headers::WaveFmt,
+    pub wave_data : headers::WaveData,
+    pub _wave : wave::SineWave,
 }
 
 impl WAVTemplate {
-    pub fn new() -> WAVTemplate {
+    pub fn new(_wave: wave::SineWave) -> WAVTemplate {
         Self {
             wave_header : headers::WaveHeader::new(),
             wave_format : headers::WaveFmt::new(),
             wave_data : headers::WaveData::new(),
+            _wave : _wave.clone(),
         }
+    }
+}
+
+impl Compile for WAVTemplate {
+    fn compile(&mut self) -> &mut Self {
+        self.wave_header.compile();
+        self.wave_format.compile();
+        self.wave_data.compile(&self._wave, &self.wave_format);
+        self
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::WAVTemplate;
+    use crate::common::Compile;
+    use crate::wave::SineWave;
+
+    #[test]
+    fn test_basic(){
+        let wv : SineWave = SineWave::new(261.626,44100,60);
+        let wt = WAVTemplate::new(wv).compile();
     }
 }
--- src/wave/mod.rs
+++ src/wave/mod.rs
@@ -2,37 +2,37 @@ use std::f64;
 
 #[derive(Clone)]
 pub struct SineWave {
-    frequency : f64,
-    sample_rate : u64, 
-    total_seconds : u64,
-    samples : Vec<i16>,     
+    pub frequency : f64,
+    pub sample_rate : u64, 
+    pub total_seconds : u64,
+    pub samples : Vec<i16>,     
 }
 
-fn calculate_sample(current_sample: i16, samples_per_cycle: f64, amplitude: u32) -> f64 {
+fn calculate_sample(current_sample: i16, samples_per_cycle: f64, amplitude: i16) -> f64 {
     (current_sample as f64 / samples_per_cycle * 2.0 * f64::consts::PI).sin() * amplitude as f64
 }
 
 impl SineWave {
     // TODO: Replace frequency with configuration
-    pub fn new(frequency: f64, sample_rate: u64, total_seconds: u64) -> SineWave {
+    pub fn new(frequency: f64, sample_rate: u64, total_seconds: u64) -> Self {
         Self {
             frequency: frequency,
             sample_rate: sample_rate,
             total_seconds: total_seconds,
             samples : vec![]
-        }.build()
+        }.build().clone()
     }
 
-    fn build(&mut self) -> SineWave {
+    fn build(&mut self) -> &mut Self {
         let total_samples = self.total_seconds * self.sample_rate;
-        let amplitude : u32 = 32000;
+        let amplitude : i16 = 32760;
         let samples_per_cycle = self.sample_rate as f64 / self.frequency;
         for i in 0..total_samples as i16 {
             let sample_value = calculate_sample(i, samples_per_cycle, amplitude);
             self.samples.push(sample_value as i16);
         }
         println!("{}", self.samples.len());
-        self.clone()
+        self
     }
 }
 
@@ -43,7 +43,7 @@ mod test {
     fn test_sine_amplitude_range() {
         let wv : SineWave = SineWave::new(261.626,44100,60);
         for sample in wv.samples {
-            assert!(sample <= 32000 && sample >= -32000);
+            assert!(sample <= 32760 && sample >= -32760);
         }
     }
 }
